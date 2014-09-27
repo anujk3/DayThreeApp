e8cc8f6a8fb6dde914b4f4a10718cbebb16641a1
Anuj Katiyal <anujk3@gmail.com>
2014-09-27 19:04:37 +0530

    Initial Commit

---
+++ DayThreeApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DayThreeApp
+//
+//  Created by Anuj Katiyal on 27/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DayThreeApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DayThreeApp
+//
+//  Created by Anuj Katiyal on 27/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
