551ca24bd9b30770b04d688f3a5291f55d3e8582
Anuj Katiyal <anujk3@gmail.com>
2014-09-28 04:30:13 +0530

    Changes made for Day 4 of the app also.

---
+++ DayThreeApp/SecondViewController.h
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.h
+//  DayThreeApp
+//
+//  Created by Anuj Katiyal on 28/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+- (IBAction)tappedClose:(id)sender;
+
+@end
---
+++ DayThreeApp/SecondViewController.m
@@ -0,0 +1,41 @@
+//
+//  SecondViewController.m
+//  DayThreeApp
+//
+//  Created by Anuj Katiyal on 28/09/14.
+//  Copyright (c) 2014 Katiyals. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)tappedClose:(id)sender {
+//    [self dismissViewControllerAnimated:YES completion:nil];
+    [self.navigationController popViewControllerAnimated:YES];
+}
+@end
